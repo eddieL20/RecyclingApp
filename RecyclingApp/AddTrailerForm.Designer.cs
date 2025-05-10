eaa516be843022acce55b81c60aa4f3f393e70dc
Eduardo Lopez <eddie56.el@gmail.com>
2025-05-09 19:39:38 -0700

    Add user input validation for AddTrailerForm

--- RecyclingApp/AddTrailerForm.Designer.cs
+++ RecyclingApp/AddTrailerForm.Designer.cs
@@ -39,130 +39,131 @@
             label4 = new Label();
             txtDockNum = new TextBox();
             btnSubmitTrailer = new Button();
-            btnExitAddTrailerForm = new Button();
+            btnCancelAddTrailer = new Button();
             SuspendLayout();
             // 
             // dtArrivalDate
             // 
             dtArrivalDate.CustomFormat = "yyyy/MM/dd hh:mm:ss";
             dtArrivalDate.Format = DateTimePickerFormat.Custom;
-            dtArrivalDate.Location = new Point(161, 156);
+            dtArrivalDate.Location = new Point( 161 , 156 );
             dtArrivalDate.Name = "dtArrivalDate";
-            dtArrivalDate.Size = new Size(250, 27);
+            dtArrivalDate.Size = new Size( 250 , 27 );
             dtArrivalDate.TabIndex = 0;
             // 
             // txtTrailerId
             // 
-            txtTrailerId.Location = new Point(161, 54);
+            txtTrailerId.Location = new Point( 161 , 54 );
             txtTrailerId.Name = "txtTrailerId";
-            txtTrailerId.Size = new Size(204, 27);
+            txtTrailerId.Size = new Size( 204 , 27 );
             txtTrailerId.TabIndex = 1;
             // 
             // lbTrailerId
             // 
             lbTrailerId.AutoSize = true;
-            lbTrailerId.Location = new Point(80, 54);
+            lbTrailerId.Location = new Point( 80 , 54 );
             lbTrailerId.Name = "lbTrailerId";
-            lbTrailerId.Size = new Size(70, 20);
+            lbTrailerId.Size = new Size( 70 , 20 );
             lbTrailerId.TabIndex = 2;
             lbTrailerId.Text = "Trailer Id:";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(54, 108);
+            label1.Location = new Point( 54 , 108 );
             label1.Name = "label1";
-            label1.Size = new Size(96, 20);
+            label1.Size = new Size( 96 , 20 );
             label1.TabIndex = 3;
             label1.Text = "Total Weight:";
             // 
             // txtTotalWeight
             // 
-            txtTotalWeight.Location = new Point(161, 105);
+            txtTotalWeight.Location = new Point( 161 , 105 );
             txtTotalWeight.Name = "txtTotalWeight";
-            txtTotalWeight.Size = new Size(204, 27);
+            txtTotalWeight.Size = new Size( 204 , 27 );
             txtTotalWeight.TabIndex = 4;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(59, 156);
+            label2.Location = new Point( 59 , 156 );
             label2.Name = "label2";
-            label2.Size = new Size(91, 20);
+            label2.Size = new Size( 91 , 20 );
             label2.TabIndex = 5;
             label2.Text = "Arrival Date:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(104, 216);
+            label3.Location = new Point( 104 , 216 );
             label3.Name = "label3";
-            label3.Size = new Size(46, 20);
+            label3.Size = new Size( 46 , 20 );
             label3.TabIndex = 6;
             label3.Text = "From:";
             // 
             // txtFromLocation
             // 
-            txtFromLocation.Location = new Point(161, 213);
+            txtFromLocation.Location = new Point( 161 , 213 );
             txtFromLocation.Name = "txtFromLocation";
-            txtFromLocation.Size = new Size(125, 27);
+            txtFromLocation.Size = new Size( 125 , 27 );
             txtFromLocation.TabIndex = 7;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(91, 265);
+            label4.Location = new Point( 91 , 265 );
             label4.Name = "label4";
-            label4.Size = new Size(59, 20);
+            label4.Size = new Size( 59 , 20 );
             label4.TabIndex = 8;
             label4.Text = "Dock #:";
             // 
             // txtDockNum
             // 
-            txtDockNum.Location = new Point(161, 265);
+            txtDockNum.Location = new Point( 161 , 265 );
             txtDockNum.Name = "txtDockNum";
-            txtDockNum.Size = new Size(125, 27);
+            txtDockNum.Size = new Size( 125 , 27 );
             txtDockNum.TabIndex = 9;
             // 
             // btnSubmitTrailer
             // 
-            btnSubmitTrailer.Location = new Point(161, 330);
+            btnSubmitTrailer.Location = new Point( 161 , 330 );
             btnSubmitTrailer.Name = "btnSubmitTrailer";
-            btnSubmitTrailer.Size = new Size(94, 29);
+            btnSubmitTrailer.Size = new Size( 94 , 29 );
             btnSubmitTrailer.TabIndex = 10;
             btnSubmitTrailer.Text = "Submit";
             btnSubmitTrailer.UseVisualStyleBackColor = true;
-            btnSubmitTrailer.Click += btnSubmitTrailer_Click;
+            btnSubmitTrailer.Click +=  btnSubmitTrailer_Click ;
             // 
-            // btnExitAddTrailerForm
+            // btnCancelAddTrailer
             // 
-            btnExitAddTrailerForm.Location = new Point(288, 330);
-            btnExitAddTrailerForm.Name = "btnExitAddTrailerForm";
-            btnExitAddTrailerForm.Size = new Size(94, 29);
-            btnExitAddTrailerForm.TabIndex = 11;
-            btnExitAddTrailerForm.Text = "Exit";
-            btnExitAddTrailerForm.UseVisualStyleBackColor = true;
+            btnCancelAddTrailer.Location = new Point( 288 , 330 );
+            btnCancelAddTrailer.Name = "btnCancelAddTrailer";
+            btnCancelAddTrailer.Size = new Size( 94 , 29 );
+            btnCancelAddTrailer.TabIndex = 11;
+            btnCancelAddTrailer.Text = "Cancel";
+            btnCancelAddTrailer.UseVisualStyleBackColor = true;
+            btnCancelAddTrailer.Click +=  btnCancelAddTrailerForm_Click ;
             // 
             // AddTrailerForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF( 8F , 20F );
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(495, 450);
-            Controls.Add(btnExitAddTrailerForm);
-            Controls.Add(btnSubmitTrailer);
-            Controls.Add(txtDockNum);
-            Controls.Add(label4);
-            Controls.Add(txtFromLocation);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(txtTotalWeight);
-            Controls.Add(label1);
-            Controls.Add(lbTrailerId);
-            Controls.Add(txtTrailerId);
-            Controls.Add(dtArrivalDate);
+            ClientSize = new Size( 495 , 450 );
+            Controls.Add( btnCancelAddTrailer );
+            Controls.Add( btnSubmitTrailer );
+            Controls.Add( txtDockNum );
+            Controls.Add( label4 );
+            Controls.Add( txtFromLocation );
+            Controls.Add( label3 );
+            Controls.Add( label2 );
+            Controls.Add( txtTotalWeight );
+            Controls.Add( label1 );
+            Controls.Add( lbTrailerId );
+            Controls.Add( txtTrailerId );
+            Controls.Add( dtArrivalDate );
             Name = "AddTrailerForm";
             Text = "AddTrailerForm";
-            ResumeLayout(false);
+            ResumeLayout( false );
             PerformLayout();
         }
 
@@ -179,6 +180,6 @@
         private Label label4;
         private TextBox txtDockNum;
         private Button btnSubmitTrailer;
-        private Button btnExitAddTrailerForm;
+        private Button btnCancelAddTrailer;
     }
 }
--- RecyclingApp/AddTrailerForm.cs
+++ RecyclingApp/AddTrailerForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RecyclingApp.Models.DataLayer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +18,66 @@ namespace RecyclingApp
             InitializeComponent();
         }
 
-        private void btnSubmitTrailer_Click(object sender, EventArgs e)
+        private void btnSubmitTrailer_Click( object sender , EventArgs e )
         {
+            validateAddTrailerTxt();
 
+
+            using ( var context = new RecycleContext() )
+            {
+
+            }
+            this.Close();
+        }
+
+        private void validateAddTrailerTxt()
+        {
+            try
+            {
+                if ( string.IsNullOrEmpty( txtTrailerId.Text ) ||
+                    string.IsNullOrEmpty( txtTotalWeight.Text ) ||
+                    string.IsNullOrEmpty( txtFromLocation.Text ) ||
+                    string.IsNullOrEmpty( txtDockNum.Text ) )
+                {
+                    throw new Exception( "All fields must be filled before submitting." );
+                }
+                if ( !int.TryParse( txtTrailerId.Text , out int trailerId ) )
+                {
+                    throw new Exception( "Trailer ID must be a valid integer." );
+                }
+                if ( !decimal.TryParse( txtTotalWeight.Text , out decimal totalWeight ) )
+                {
+                    throw new Exception( "Total Weight must be a valid decimal number." );
+                }
+                if ( txtFromLocation.Text.Trim().Length == 0 )
+                {
+                    throw new Exception( "From Location cannot be empty or whitespace." );
+                }
+                if ( !int.TryParse( txtDockNum.Text , out int dockNum ) )
+                {
+                    throw new Exception( "Dock # must be a valid integer." );
+                }
+
+                MessageBox.Show(
+                    "Validation successful!" ,
+                    "Success" ,
+                    MessageBoxButtons.OK ,
+                    MessageBoxIcon.Information );
+
+            } catch ( Exception ex )
+            {
+                MessageBox.Show(
+                    ex.Message ,
+                    "Validation Error" ,
+                    MessageBoxButtons.OK ,
+                    MessageBoxIcon.Error );
+
+            }
+        }
+
+        private void btnCancelAddTrailerForm_Click( object sender , EventArgs e )
+        {
+            this.Close();
         }
     }
 }
